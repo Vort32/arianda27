2968364fb7715218ef30868bfddd1d1ad9d783ee
Arsa <91706224+Vort32@users.noreply.github.com>
2023-10-04 21:59:02 +0800

    Add files via upload

---
+++ Posttest3/gelap.js
@@ -0,0 +1,41 @@
+var nama = prompt("Nama Anda ?")
+alert("Selama datang di Kivotos " + nama )  
+console.log("Selamat Datang Sensei"); 
+
+
+function toggleDarkMode() {
+    let dark = document.getElementById('darkmode-toggle').checked;
+    const medsos = document.querySelector('.medsos');
+    const banner = document.querySelector('.banner');
+
+    if (dark) {
+        medsos.classList.remove('medsos');
+        medsos.classList.add('medsos_dark');
+
+        banner.classList.remove('banner');
+        banner.classList.add('banner_dark');
+    } 
+    else{
+        medsos.classList.remove('medsos_dark');
+        medsos.classList.add('medsos');
+
+        banner.classList.remove('banner_dark');
+        banner.classList.add('banner');
+    }
+}
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('darkmode-toggle');
+
+    darkModeToggle.checked = localStorage.getItem('darkMode') === 'true';
+
+    darkModeToggle.addEventListener('change', function () {
+        toggleDarkMode();
+        localStorage.setItem('darkMode', darkModeToggle.checked);
+    });
+
+    if (darkModeToggle.checked) {
+        toggleDarkMode();
+    }
+});
