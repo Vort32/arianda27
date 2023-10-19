ee47ca178399e7716c97d9a01d720762bed94245
Arsa <91706224+Vort32@users.noreply.github.com>
2023-10-19 22:36:33 +0800

    Add files via upload

---
+++ Posttest5/crud/index.php
@@ -0,0 +1,66 @@
+<?php
+include 'koneksi.php';
+
+$banners = [];
+
+$result = $conn->query("SELECT * FROM ba");
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $banners[] = $row;
+    }
+}
+
+function displayBanner($banner)
+{
+    echo '<div class="banner">
+            <div class="banner-wrapper">
+                <img src="uploads/' . $banner['image_name'] . '" alt="' . $banner['nama'] . '">
+                <div class="banner-description">
+                    <h2>' . $banner['nama'] . '</h2>
+                    <p>' . $banner['school'] . '</p>
+                    <p>' . $banner['rarity'] . '</p>
+                    <p>' . $banner['role'] . '</p>
+                    <p>' . $banner['weapon_type'] . '</p>
+                    <p>' . $banner['armor_type'] . '</p>
+                    <div class="banner-actions">
+                        <a href="edit.php?id=' . $banner['id'] . '">
+                            <button>Edit</button>
+                        </a>
+                        <a href="hapus.php?id=' . $banner['id'] . '">
+                            <button>Delete</button>
+                        <a href="tambah.php?id=' . $banner['id'] . '">
+                            <button>Tambah</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tampilan Char</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="shortcut-link">
+    <a href="tambah.php">Tambah Karakter</a>
+</div>
+
+<div class="shortcut-link">
+    <a href="../index.php">HOME</a>
+</div>
+<section id="home" class="body-content">    
+        <?php
+        foreach ($banners as $banner) {
+            displayBanner($banner);
+        }
+        ?>
+</section>
+</body>
+</html>
